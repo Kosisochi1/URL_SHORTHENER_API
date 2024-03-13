840bd9f0872e1773b594c971959612a0acd95961
emmanuel <emmanuelezeoyirikosi@yahoo.com>
2024-03-13 22:33:13 +0100

    cors

--- src/api.ts
+++ src/api.ts
@@ -39,6 +39,11 @@ const corsOptions: CorsOptions = {
     allowedHeaders: ['Content-Type:application/json'],
     credentials: true
 };
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
