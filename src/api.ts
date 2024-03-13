e27bde8a8092b6ab5249fbfd2c93e97d47855928
emmanuel <emmanuelezeoyirikosi@yahoo.com>
2024-03-14 00:16:58 +0100

    cors

--- src/api.ts
+++ src/api.ts
@@ -33,21 +33,21 @@ interface CorsOptions {
 }
 
 // Example usage
-const corsOptions: CorsOptions = {
-    origin: 'https://url-shorthener-api.onrender.com',
-    methods: ['GET', 'POST','PATCH','DELETE'],
-    allowedHeaders: ['Content-Type:application/json'],
-    credentials: true
-};
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-  })
+// const corsOptions: CorsOptions = {
+//     origin: 'https://url-shorthener-api.onrender.com',
+//     methods: ['GET', 'POST','PATCH','DELETE'],
+//     allowedHeaders: ['Content-Type:application/json'],
+//     credentials: true
+// };
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+//   })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/public' ,express.static(path.join(__dirname,'public')));
 app.use('/public' ,express.static(path.join(__dirname,'reset.html')));
